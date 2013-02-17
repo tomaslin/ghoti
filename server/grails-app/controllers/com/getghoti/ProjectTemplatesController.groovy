9d91fa2c647eced73f53dd3fd2e1e729ff7b04c3
Tomas Lin <tomaslin@gmail.com>
2013-02-17 01:33:07 +0000

    removed server

--- server/grails-app/controllers/com/getghoti/ProjectTemplatesController.groovy
+++
@@ -1,9 +0,0 @@
-package com.getghoti
-
-import grails.plugins.springsecurity.Secured
-import org.springframework.dao.DataIntegrityViolationException
-
-@Secured(['ROLE_ADMIN'])
-class ProjectTemplatesController {
-    static scaffold = true
-}
